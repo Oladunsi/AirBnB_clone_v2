b6906d8332ce44a61576536ed72005c933c67548
Oladunsi <okeoladunsi6@gmail.com>
2023-05-20 11:47:40 +0000

    refactoring my first flask app properly

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
-    return "Hello HBNB!$")
+    return "Hello HBNB!$"
+
+if __name__ == "__main__":
+    app.run()
