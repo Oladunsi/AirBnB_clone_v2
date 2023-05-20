69fb6105f1a2403ea9ed9a5633fc7931ad52f168
Oladunsi <okeoladunsi6@gmail.com>
2023-05-20 12:27:17 +0000

    added /HNBN route to flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,7 +14,11 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    return "Hello HBNB!$"
+    return "Hello HBNB!"
+
+@app.route("/HBNB", strict_slashes=False)
+def hello_world2():
+    return "Hello HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
